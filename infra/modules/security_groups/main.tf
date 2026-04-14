77f94992aaa91f2f7ad54166de4a9fffbe5a4e0b
Volodymyr Lazurenko <volodymyr.lazurenko@namecheap.com>
2026-04-14 22:00:56 +0300

    refactor(infra): replace ingress/egress inline blocks with according vpc rule resources

--- infra/modules/security_groups/main.tf
+++ infra/modules/security_groups/main.tf
@@ -1,7 +1,7 @@
 locals {
   egress_all = {
     description = "Allow all outbound traffic"
-    from_port   = 0
+    from_port   = 0 # `from` and `to` _port rules are ignored when `-1` protocol is defined
     to_port     = 0
     protocol    = "-1"
   }
@@ -58,33 +58,38 @@ resource "aws_security_group" "ec2" {
   description = "Allow app traffic from SSH from Bastion and ALB"
   vpc_id      = var.vpc_id
 
-  ingress {
-    description = "App port from ALB"
-    from_port   = var.app_port
-    to_port     = var.app_port
-    protocol    = "tcp"
-    cidr_blocks = [aws_security_group.alb.id]
-  }
-
-  ingress {
-    description = "SSH from Bastion"
-    from_port   = local.default_ssh_port
-    to_port     = local.default_ssh_port
-    protocol    = "tcp"
-    cidr_blocks = [aws_security_group.bastion.id]
-  }
-
-  egress {
-    description = local.egress_all.description
-    from_port   = local.egress_all.from_port
-    to_port     = local.egress_all.to_port
-    protocol    = local.egress_all.protocol
-    cidr_blocks = [local.cidr_default]
-  }
-
   tags = merge(var.tags, {
     Name = "${var.project_name}-${var.environment}-sg-ec2"
   })
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_inbound" {
+  security_group_id = aws_security_group.ec2.id
+  description       = "App port from ALB"
+  ip_protocol       = local.tcp_protocol
+  from_port         = var.app_port
+  to_port           = var.app_port
+
+  referenced_security_group_id = aws_security_group.alb.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_inbound_ssh" {
+  security_group_id = aws_security_group.ec2.id
+  description       = "SSH from Bastion"
+  ip_protocol       = local.tcp_protocol
+  from_port         = local.default_ssh_port
+  to_port           = local.default_ssh_port
+
+  referenced_security_group_id = aws_security_group.bastion.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "ec2_outbound" {
+  security_group_id = aws_security_group.ec2.id
+  description       = local.egress_all.description
+  from_port         = local.egress_all.from_port
+  to_port           = local.egress_all.to_port
+  ip_protocol       = local.egress_all.protocol
+  cidr_ipv4         = local.cidr_default
 }
 
 # ---------------- RDS Security Group ------------------
@@ -93,26 +98,30 @@ resource "aws_security_group" "rds" {
   description = "Allow RDS traffic from EC2"
   vpc_id      = var.vpc_id
 
-  ingress {
-    description = "PostgreSQL from EC2"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = [aws_security_group.ec2.id]
-  }
-
-  egress {
-    description = local.egress_all.description
-    from_port   = local.egress_all.from_port
-    to_port     = local.egress_all.to_port
-    protocol    = local.egress_all.protocol
-    cidr_blocks = [local.cidr_default]
-  }
-
   tags = merge(var.tags, {
     Name = "${var.project_name}-${var.environment}-sg-rds"
   })
 }
+resource "aws_vpc_security_group_ingress_rule" "rds_inbound" {
+  description       = "PostgreSQL from EC2"
+  security_group_id = aws_security_group.rds.id
+  ip_protocol       = local.tcp_protocol
+  from_port         = 5432
+  to_port           = 5432
+
+  referenced_security_group_id = aws_security_group.ec2.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "rds_outbound" {
+  security_group_id = aws_security_group.rds.id
+
+  description = local.egress_all.description
+  from_port   = local.egress_all.from_port
+  to_port     = local.egress_all.to_port
+  ip_protocol = local.egress_all.protocol
+  cidr_ipv4   = local.cidr_default
+}
+
 
 # ---------------- Bastion Security Group ------------------
 resource "aws_security_group" "bastion" {
@@ -137,9 +146,9 @@ resource "aws_vpc_security_group_ingress_rule" "bastion_inbound_ssh" {
 resource "aws_vpc_security_group_egress_rule" "bastion_outbound" {
   security_group_id = aws_security_group.bastion.id
   description       = local.egress_all.description
-  ip_protocol       = "tcp"
-  from_port         = local.default_ssh_port
-  to_port           = local.default_ssh_port
+  ip_protocol       = local.egress_all.protocol
+  from_port         = local.egress_all.from_port
+  to_port           = local.egress_all.to_port
   cidr_ipv4         = local.cidr_default
 }
 
